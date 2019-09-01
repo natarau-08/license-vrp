a6a6b24728303e94d1653d0e82d60686326f75a9
natarau-08 <alexandru.tdgf2013@gmail.com>
2019-09-01 21:46:40 +0300

    forgot graph parser
    - forgot the Graphparser

---
+++ src/algorithm/GraphParser.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+import database.obj.cvrp.CvrpGraph;
+
+/**
+ * Transforms graphs into a set of matrices
+ * @author Alexandru
+ */
+public class GraphParser {
+
+	/**
+	 * Converts database objects into matrices for CvrpGraph
+	 * @return - array af matrices. First matrix contains the demands. The second contains travel costs
+	 */
+	public static int[][][] parseCvrpGraph(CvrpGraph graph){
+		return null;
+	}
+}
